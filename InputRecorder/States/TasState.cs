37e4bdc7ce02ee8a99f6cd5c1ea6f3b524733617
JeFi <62518788+JeFi-314@users.noreply.github.com>
2024-12-24 11:31:36 +0800

    Change input indention to JK studio format

--- InputRecorder/States/TasState.cs
+++ InputRecorder/States/TasState.cs
@@ -5,7 +5,6 @@ namespace InputRecorder.States;
 public class TasState
 {
     public int Ticks;
-    private int Indent = 2;
     public PadState PrintState;
 
     public TasState(PadState padState, int ticks = 1){
@@ -22,7 +21,7 @@ public class TasState
     public string ToTasString() {
         List<string> elements = new();
 
-        elements.Add(Ticks.ToString());
+        elements.Add($"{Ticks,4}");
         if (PrintState.up) {elements.Add(InputRecorder.TasBinding.up);}
         if (PrintState.down) {elements.Add(InputRecorder.TasBinding.down);}
         if (PrintState.left) {elements.Add(InputRecorder.TasBinding.left);}
@@ -35,6 +34,6 @@ public class TasState
         if (PrintState.snake) {elements.Add(InputRecorder.TasBinding.snake);}
         if (PrintState.restart) {elements.Add(InputRecorder.TasBinding.restart);}
 
-        return new string(' ', Indent) + string.Join(",", elements);
+        return string.Join(",", elements);
     }
 }
