8698e36af6856dfa4dae037a80ac39fc05c0304f
JeFi <62518788+JeFi-314@users.noreply.github.com>
2025-01-06 15:59:21 +0800

    if no tas code then skipped in tas writing

--- InputRecorder/States/TasState.cs
+++ InputRecorder/States/TasState.cs
@@ -6,7 +6,6 @@ public class TasState
 {
     public int Ticks;
     public PadState PrintState;
-
     public TasState(PadState padState, int ticks = 1){
         PrintState = padState;
         Ticks = ticks;
@@ -22,17 +21,17 @@ public class TasState
         List<string> elements = new();
 
         elements.Add($"{Ticks,4}");
-        if (PrintState.up) {elements.Add(InputRecorder.TasBinding.up);}
-        if (PrintState.down) {elements.Add(InputRecorder.TasBinding.down);}
-        if (PrintState.left) {elements.Add(InputRecorder.TasBinding.left);}
-        if (PrintState.right) {elements.Add(InputRecorder.TasBinding.right);}
-        if (PrintState.jump) {elements.Add(InputRecorder.TasBinding.jump);}
-        if (PrintState.confirm) {elements.Add(InputRecorder.TasBinding.confirm);}
-        if (PrintState.cancel) {elements.Add(InputRecorder.TasBinding.cancel);}
-        if (PrintState.pause) {elements.Add(InputRecorder.TasBinding.pause);}
-        if (PrintState.boots) {elements.Add(InputRecorder.TasBinding.boots);}
-        if (PrintState.snake) {elements.Add(InputRecorder.TasBinding.snake);}
-        if (PrintState.restart) {elements.Add(InputRecorder.TasBinding.restart);}
+        if (PrintState.up && InputRecorder.TasBinding.up!=string.Empty) {elements.Add(InputRecorder.TasBinding.up);}
+        if (PrintState.down && InputRecorder.TasBinding.down!=string.Empty) {elements.Add(InputRecorder.TasBinding.down);}
+        if (PrintState.left && InputRecorder.TasBinding.left!=string.Empty) {elements.Add(InputRecorder.TasBinding.left);}
+        if (PrintState.right && InputRecorder.TasBinding.right!=string.Empty) {elements.Add(InputRecorder.TasBinding.right);}
+        if (PrintState.jump && InputRecorder.TasBinding.jump!=string.Empty) {elements.Add(InputRecorder.TasBinding.jump);}
+        if (PrintState.confirm && InputRecorder.TasBinding.confirm!=string.Empty) {elements.Add(InputRecorder.TasBinding.confirm);}
+        if (PrintState.cancel && InputRecorder.TasBinding.cancel!=string.Empty) {elements.Add(InputRecorder.TasBinding.cancel);}
+        if (PrintState.pause && InputRecorder.TasBinding.pause!=string.Empty) {elements.Add(InputRecorder.TasBinding.pause);}
+        if (PrintState.boots && InputRecorder.TasBinding.boots!=string.Empty) {elements.Add(InputRecorder.TasBinding.boots);}
+        if (PrintState.snake && InputRecorder.TasBinding.snake!=string.Empty) {elements.Add(InputRecorder.TasBinding.snake);}
+        if (PrintState.restart && InputRecorder.TasBinding.restart!=string.Empty) {elements.Add(InputRecorder.TasBinding.restart);}
 
         return string.Join(",", elements);
     }
